3a79302326c47858274543878ecd71693c5e812d
Andrii Danylovskyi <adanylo@softserveinc.com>
2023-09-05 20:53:26 +0300

    UC#12 Create documentation

--- app/src/main/java/com/example/javadocumantation/Utils.java
+++ app/src/main/java/com/example/javadocumantation/Utils.java
@@ -1,14 +1,31 @@
+package com.example.javadocumantation;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * A utility class providing various string manipulation methods.
+ */
 public class Utils {
 
     private static final int INDEX_NOT_FOUND = -1;
     private static final String EMPTY = "";
 
+    /**
+     * Abbreviates a string by given lower and upper boundaries and appends a specified string at the end.
+     *
+     * @param str          The string to abbreviate.
+     * @param lower        The lower boundary for abbreviation.
+     * @param upper        The upper boundary for abbreviation.
+     * @param appendToEnd  String to append to the end of the abbreviated string.
+     * @return The abbreviated string.
+     *
+     * Example:
+     * abbreviate("Hello World", 3, 7, "...") => "Hel..."
+     */
     public static String abbreviate(final String str, int lower, int upper, final String appendToEnd) {
         isTrue(upper >= -1, "upper value cannot be less than -1");
         isTrue(upper >= lower || upper == -1, "upper value is less than lower value");
@@ -39,6 +56,16 @@ public class Utils {
         return result.toString();
     }
 
+    /**
+     * Extracts initials from a given string, using the specified delimiters to identify word boundaries.
+     *
+     * @param str        The string from which to extract initials.
+     * @param delimiters Characters used to identify word boundaries.
+     * @return A string containing the initials.
+     *
+     * Example:
+     * initials("John A. Doe", ' ', '.') => "JAD"
+     */
     public static String initials(final String str, final char... delimiters) {
         if (isEmpty(str)) {
             return str;
@@ -66,6 +93,15 @@ public class Utils {
         return new String(newCodePoints, 0, count);
     }
 
+    /**
+     * Swaps the case of a string, converting uppercase to lowercase and vice versa.
+     *
+     * @param str The string whose case is to be swapped.
+     * @return The string with its case swapped.
+     *
+     * Example:
+     * swapCase("Hello World") => "hELLO wORLD"
+     */
     public static String swapCase(final String str) {
         if (isEmpty(str)) {
             return str;
@@ -97,6 +133,24 @@ public class Utils {
         return new String(newCodePoints, 0, outOffset);
     }
 
+    /**
+     * Wraps a string based on the specified wrap length, new line string, and a string to wrap on.
+     *
+     * @param str           The string to wrap.
+     * @param wrapLength    The length to wrap at.
+     * @param newLineStr    The string to use for line breaks.
+     * @param wrapLongWords Indicates if long words should be wrapped.
+     * @param wrapOn        The string to wrap on.
+     * @return The wrapped string.
+     *
+     * Example:
+     * wrap("Hello World, have a nice day!", 10, "\n", true, " ") =>
+     * "Hello
+     * World,
+     * have a
+     * nice
+     * day!"
+     */
     public static String wrap(final String str,
                               int wrapLength,
                               String newLineStr,
@@ -190,6 +244,12 @@ public class Utils {
         return wrappedLine.toString();
     }
 
+    /**
+     * Generates a set of delimiters.
+     *
+     * @param delimiters Array of delimiter characters.
+     * @return A set containing code points of the delimiters.
+     */
     private static Set<Integer> generateDelimiterSet(final char[] delimiters) {
         final Set<Integer> delimiterHashSet = new HashSet<>();
         if (delimiters == null || delimiters.length == 0) {
@@ -206,20 +266,44 @@ public class Utils {
         return delimiterHashSet;
     }
 
+    /**
+     * Checks if an expression is true and throws an exception with the provided message if not.
+     *
+     * @param expression Boolean expression to be checked.
+     * @param message    Exception message if the expression is false.
+     */
     private static void isTrue(final boolean expression, final String message) {
         if (!expression) {
             throw new IllegalArgumentException(message);
         }
     }
 
+    /**
+     * Returns the length of the provided char sequence or 0 if it's null.
+     *
+     * @param cs The character sequence.
+     * @return The length of the character sequence.
+     */
     private static int length(final CharSequence cs) {
         return cs == null ? 0 : cs.length();
     }
 
+    /**
+     * Checks if a char sequence is empty.
+     *
+     * @param cs The character sequence to check.
+     * @return true if the char sequence is empty or null, false otherwise.
+     */
     private static boolean isEmpty(final CharSequence cs) {
         return cs == null || cs.length() == 0;
     }
 
+    /**
+     * Checks if a char sequence is blank (empty or only contains whitespace).
+     *
+     * @param cs The character sequence to check.
+     * @return true if the char sequence is blank, false otherwise.
+     */
     private static boolean isBlank(final CharSequence cs) {
         final int strLen = length(cs);
         if (strLen == 0) {
@@ -233,10 +317,24 @@ public class Utils {
         return true;
     }
 
+    /**
+     * Returns the string itself or an empty string if the input is null.
+     *
+     * @param str The string to check.
+     * @return The original string or an empty string if it's null.
+     */
     private static String defaultString(final String str) {
         return Objects.toString(str, EMPTY);
     }
 
+    /**
+     * Finds the index of a sub-sequence in a char sequence starting from a specified position.
+     *
+     * @param seq       The char sequence to be searched.
+     * @param searchSeq The sub-sequence to find.
+     * @param startPos  The starting position for the search.
+     * @return The index of the sub-sequence or -1 if not found.
+     */
     private static int indexOf(final CharSequence seq, final CharSequence searchSeq, final int startPos) {
         if (seq == null || searchSeq == null) {
             return INDEX_NOT_FOUND;
